18047414e72a56be32eaf9b3298e2a86acbbfeca
krasno86 <krasno86@mail.ru>
2018-05-11 14:22:36 +0300

    comlite lesson 1

---
+++ src/Article.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import articles from './fixtures'
+
+class Article extends React.Component {
+	state = {
+		isOpen: true
+	}
+
+	render() {
+		const {article} = this.props
+        const {isOpen} = this.state
+		// const body = this.state.isOpen && <section>{article.text}</section>
+		return (
+			<div>
+				<h2>{article.title}
+					<button onClick={this.handleClick}>
+						{isOpen ? 'close' : 'open'}
+					</button>
+				</h2>
+				{this.getBody()}
+			</div>
+		)
+	}
+
+    getBody() {
+        if (!this.state.isOpen) return null
+        const {article} = this.props
+        return  <section>{article.text}</section>
+    }
+
+	handleClick = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+}
+
+// function Article(props) {
+// 	const {article} = props
+//     return  <div>
+// 				<h3>Имя: {article.name}</h3>
+// 				<section>Возраст: {article.text}</section>
+//     		</div>
+// }
+	
+export default Article
---
+++ src/ArticleList.js
@@ -0,0 +1,47 @@
+// import React, {Component} from 'react'
+import React from 'react'
+import Article from './Article'
+
+// class ArticleList extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             isOpen: false
+//         }
+//     }
+//
+// 	render() {
+//
+//         const body = this.state.isOpen && <h2>{propsValues.items}</h2>
+//
+//         return (
+// 	    	<div>
+// 	    		<h2>
+//                     {propsValues.title}
+//                     <button onClick={this.handleClick}>
+//                         {this.state.isOpen ? 'close': 'open'}
+//                     </button>
+//                 </h2>
+//                 {body}
+//                 <ul>
+//                     {itemElements}
+//                 </ul>
+//     		</div>
+// 	    )
+//     }
+//
+//     handleClick = () => {
+//         console.log('ergeghergh')
+//         this.setState({
+//             isOpen: !this.state.isOpen
+//         })
+//     }
+// }
+
+function ArticleList({articles}) {
+    console.log(articles)
+	const articleElements = articles.map((article) => <li key = {article.id}><Article article = {article}/></li>)
+    return  <ul>{articleElements}</ul>
+}
+
+export default ArticleList
